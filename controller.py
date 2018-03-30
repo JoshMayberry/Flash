84e3865d8df76648fb99a0fd3137d0857d65ce59
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2018-03-30 08:20:39 -0400

    Expanded the windows for the GUI_Builder Made all windows share the same routine for adding the menu bar

--- controller.py
+++ controller.py
@@ -16,8 +16,18 @@ class GUI_Builder():
 
 	@GUI_Maker.wrap_showError(makeDialog = not isMain)
 	def buildGUI(self):
+		"""The GUI build routine.
+
+		Example Input: buildGUI()
+		"""
+
 		self.createWindows()
+		
 		self.buildMainWindow()
+		self.buildEditCards()
+		self.buildEditFormats()
+		self.buildViewCards()
+		self.buildPractice()
 
 	def createWindows(self):
 		"""Creates all the window frames before they are built.
@@ -26,7 +36,67 @@ class GUI_Builder():
 		Example Input: createWindows()
 		"""
 
-		self.frame_0  = self.gui.addWindow(label = 0,  title = f"Label Printer v.{__version__}")
+		self.frame_mainMenu     = self.gui.addWindow(label = "mainMenu",    title = f"Main Menu v.{__version__}")
+		self.frame_editCards    = self.gui.addWindow(label = "editCards",   title = f"Edit Cards v.{__version__}")
+		self.frame_editFormats  = self.gui.addWindow(label = "editFormats", title = f"Edit Formats v.{__version__}")
+		self.frame_viewCards    = self.gui.addWindow(label = "viewCards",   title = f"View Cards v.{__version__}")
+		self.frame_practice     = self.gui.addWindow(label = "practice",    title = f"Practice! v.{__version__}")
+		# self.frame_flashCard    = self.gui.addWindow(label = "flashCard",   title = f"Flash Card v.{__version__}")
+
+	def addCommonMenu(self, myFrame):
+		"""Makes a common menu for all frames.
+
+		Example Input: addCommonMenu(self.frame_mainMenu)
+		"""
+		with myFrame.addMenu(text = "&File") as myMenu:
+			with myMenu.addMenuItem(text = "Exit") as myMenuItem:
+				myMenuItem.addToolTip("Closes the software")
+				myMenuItem.setFunction_click(myFunction = myFrame.onExit)
+			
+			with myMenu.addMenuItem(text = "Save", label = "menuUpdateLocalDatabase") as myMenuItem:
+				myMenuItem.addToolTip("Save changes made to local database")
+			# 	myMenuItem.setFunction_click(myFunction = self.onSave)
+
+			myMenu.addMenuSeparator()
+
+			if (myFrame != self.frame_mainMenu):
+				with myMenu.addMenuItem(text = "Main Menu") as myMenuItem:
+					myMenuItem.addToolTip("Returns to the main menu")
+					myMenuItem.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_mainMenu)
+
+			if (myFrame != self.frame_editCards):
+				with myMenu.addMenuItem(text = "Edit Cards") as myMenuItem:
+					myMenuItem.addToolTip("Switches to the card edit window")
+					myMenuItem.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_editCards)
+
+			if (myFrame != self.frame_editFormats):
+				with myMenu.addMenuItem(text = "Edit Formats") as myMenuItem:
+					myMenuItem.addToolTip("Switches to the format edit window")
+					myMenuItem.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_editFormats)
+
+			if (myFrame != self.frame_practice):
+				with myMenu.addMenuItem(text = "Practice!") as myMenuItem:
+					myMenuItem.addToolTip("Switches to the practice window")
+					myMenuItem.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_practice)
+
+		with myFrame.addMenu(text = "&Security") as myMenu:
+			with myMenu.addMenuItem(text = "Login", label = "menuLogin") as myMenuItem:
+				myMenuItem.addToolTip("Allows the user to edit things and enables locked options")
+				# myMenuItem.setFunction_click(myFunction = self.onLogin)
+			
+			with myMenu.addMenuItem(text = "Logout", label = "menuLogout") as myMenuItem:
+				myMenuItem.addToolTip("Keeps the user from editing things and disables some options")
+				# myMenuItem.setFunction_click(myFunction = self.onLogout)
+
+		with myFrame.addMenu(text = "&Settings") as myMenu:
+			with myMenu.addMenuItem(text = "User Settings", label = "menuUserSettings") as myMenuItem:
+				myMenuItem.addToolTip("View and change various parameters for how this software operates")
+				# myMenuItem.setFunction_click(myFunction = self.frame_16.onShowWindow)
+
+		# with myFrame.addMenu(text = "&Utilities") as myMenu:			
+		# 	with myMenu.addMenuItem(text = "Debugging", label = "menuDebugging", check = True, default = self.debugging, enabled = self.enableDebugging) as myMenuItem:
+		# 		myMenuItem.addToolTip("If debugging information should be printed to the cmd window and/or written in the cmd log")
+		# 		myMenuItem.setFunction_click(myFunction = self.onSetDebugging)
 
 	def buildMainWindow(self):
 		"""Creates the main window.
@@ -34,66 +104,140 @@ class GUI_Builder():
 		Example Input: buildMainWindow()
 		"""
 
-		with self.frame_0 as myFrame:
+		with self.frame_mainMenu as myFrame:
 			#Initialize GUI
-			myFrame.setMinimumFrameSize(size = (500, 300))
-			myFrame.setWindowSize(800, 700)
+			myFrame.setMinimumFrameSize(size = (200, 200))
 			myFrame.addStatusBar()
 			# myFrame.setStatusText(self.defaultStatus)
 
 			#Build menu bar
-			with myFrame.addMenu(text = "&File") as myMenu:
-				with myMenu.addMenuItem(text = "Exit") as myMenuItem:
-					myMenuItem.addToolTip("Closes the software")
-					myMenuItem.setFunction_click(myFunction = "self.onExit")
-
-				# myMenu.addMenuSeparator()
-				
-				# with myMenu.addMenuItem(text = "]Save", label = "menuUpdateLocalDatabase") as myMenuItem:
-				# 	myMenuItem.addToolTip("Load the database from a remote computer to this computer")
-				# 	myMenuItem.setFunction_click(myFunction = self.onSave)
-
-			with myFrame.addMenu(text = "&Security") as myMenu:
-				with myMenu.addMenuItem(text = "Login", label = "menuLogin") as myMenuItem:
-					myMenuItem.addToolTip("Allows the user to edit things and enables locked options")
-					# myMenuItem.setFunction_click(myFunction = self.onLogin)
-				
-				with myMenu.addMenuItem(text = "Logout", label = "menuLogout") as myMenuItem:
-					myMenuItem.addToolTip("Keeps the user from editing things and disables some options")
-					# myMenuItem.setFunction_click(myFunction = self.onLogout)
-				
-				myMenu.addMenuSeparator()
-				
-				with myMenu.addMenuItem(text = "Password Permissions", label = "menuPasswordPermissions") as myMenuItem:
-					myMenuItem.addToolTip("View and change what permissions different users have")
-					# myMenuItem.setFunction_click(myFunction = self.frame_21.onShowWindow)
-				
-				with myMenu.addMenuItem(text = "Generate Temporary Password", label = "menuPasswordTemp") as myMenuItem:
-					myMenuItem.addToolTip("Generate temporary passwords that can be used on different computers")
-					# myMenuItem.setFunction_click(myFunction = self.frame_22.onShowWindow)
-
-			with myFrame.addMenu(text = "&Settings") as myMenu:
-				with myMenu.addMenuItem(text = "User Settings", label = "menuUserSettings") as myMenuItem:
-					myMenuItem.addToolTip("View and change various parameters for how this software operates")
-					# myMenuItem.setFunction_click(myFunction = self.frame_16.onShowWindow)
-				
-				myMenu.addMenuSeparator()
-				
-				with myMenu.addMenuItem(text = "Change Password", label = "menuChangePassword") as myMenuItem:
-					myMenuItem.addToolTip("Change the password to edit and unlock certain options")
-					# myMenuItem.setFunction_click(myFunction = self.onChangePassword)
-
-			# with myFrame.addMenu(text = "&Utilities") as myMenu:			
-			# 	with myMenu.addMenuItem(text = "Debugging", label = "menuDebugging", check = True, default = self.debugging, enabled = self.enableDebugging) as myMenuItem:
-			# 		myMenuItem.addToolTip("If debugging information should be printed to the cmd window and/or written in the cmd log")
-			# 		myMenuItem.setFunction_click(myFunction = self.onSetDebugging)
+			self.addCommonMenu(myFrame)
 
 			#Setup for content
-			with myFrame.addSizerGridFlex(rows = 1, columns = 1) as mainSizer:
+			with myFrame.addSizerGridFlex(rows = 4, columns = 1) as mainSizer:
 				mainSizer.growFlexColumnAll()
 				mainSizer.growFlexRowAll()
 
-				mainSizer.addText("Lorem")
+				with mainSizer.addButton("Edit Cards") as myWidget:
+					myWidget.addToolTip("Edit, add, and remove cards")
+					myWidget.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_editCards)
+
+				with mainSizer.addButton("Edit Formats") as myWidget:
+					myWidget.addToolTip("Edit, add, and remove formats")
+					myWidget.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_editFormats)
+
+				with mainSizer.addButton("View Cards") as myWidget:
+					myWidget.addToolTip("View all cards in this deck")
+					myWidget.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_editCards)
+
+				with mainSizer.addButton("Practice!") as myWidget:
+					myWidget.addToolTip("Use the created flash cards")
+					myWidget.setFunction_click(myFunction = myFrame.onSwitchWindow, myFunctionArgs = self.frame_practice)
+
+	def buildEditCards(self):
+		"""Edit, add, and remove cards from here.
+
+		Example Input: buildEditCards()
+		"""
+
+		with self.frame_editCards as myFrame:
+			#Initialize GUI
+			myFrame.setMinimumFrameSize(size = (200, 200))
+			myFrame.addStatusBar()
+			# myFrame.setStatusText(self.defaultStatus)
+
+			#Build menu bar
+			self.addCommonMenu(myFrame)
+
+			#Setup for content
+			with myFrame.addSizerGridFlex(rows = 3, columns = 1) as mainSizer:
+				mainSizer.growFlexColumnAll()
+				mainSizer.growFlexRowAll()
+
+				topSizer, bottomSizer = mainSizer.addSplitterDouble(minimumSize = 20, vertical = True, dividerPosition = 130, dividerGravity = 0, panel_0 = {"border": "raised"}, panel_1 = {"border": "raised"})
+
+				with topSizer as mySizer:
+					mySizer.addText("List all cards here")
+
+				with bottomSizer as mySizer:
+					mySizer.addText("Edit data fields here in a grid")
+
+				mainSizer.addButton("Show Selected Card")
+
+	def buildEditFormats(self):
+		"""Edit, add, and remove cards from here.
+
+		Example Input: buildEditFormats()
+		"""
+
+		with self.frame_editFormats as myFrame:
+			#Initialize GUI
+			myFrame.setMinimumFrameSize(size = (200, 200))
+			myFrame.addStatusBar()
+			# myFrame.setStatusText(self.defaultStatus)
+
+			#Build menu bar
+			self.addCommonMenu(myFrame)
+
+			#Setup for content
+			with myFrame.addSizerGridFlex(rows = 3, columns = 1) as mainSizer:
+				mainSizer.growFlexColumnAll()
+				mainSizer.growFlexRowAll()
+
+				topSizer, bottomSizer = mainSizer.addSplitterDouble(minimumSize = 20, vertical = True, dividerPosition = 130, dividerGravity = 0, panel_0 = {"border": "raised"}, panel_1 = {"border": "raised"})
+
+				with topSizer as mySizer:
+					mySizer.addText("List all formats here")
+
+				with bottomSizer as mySizer:
+					mySizer.addText("Edit data fields here in a grid")
+
+				mainSizer.addButton("Preview Selected Format")
+
+	def buildViewCards(self):
+		"""View the flash cards.
+
+		Example Input: buildViewCards()
+		"""
+
+		with self.frame_viewCards as myFrame:
+			#Initialize GUI
+			myFrame.setMinimumFrameSize(size = (200, 200))
+			myFrame.addStatusBar()
+			# myFrame.setStatusText(self.defaultStatus)
+
+			#Build menu bar
+			self.addCommonMenu(myFrame)
+
+			#Setup for content
+			with myFrame.addSizerGridFlex(rows = 3, columns = 1) as mainSizer:
+				mainSizer.growFlexColumnAll()
+				mainSizer.growFlexRowAll()
+
+				mainSizer.addText("List all cards here")
+
+	def buildPractice(self):
+		"""Use the flash cards.
+
+		Example Input: buildPractice()
+		"""
+
+		with self.frame_practice as myFrame:
+			#Initialize GUI
+			myFrame.setMinimumFrameSize(size = (200, 200))
+			myFrame.addStatusBar()
+			# myFrame.setStatusText(self.defaultStatus)
+
+			#Build menu bar
+			self.addCommonMenu(myFrame)
+
+			#Setup for content
+			with myFrame.addSizerGridFlex(rows = 3, columns = 1) as mainSizer:
+				mainSizer.growFlexColumnAll()
+				mainSizer.growFlexRowAll()
+
+				mainSizer.addButton("Set Rules")
+				mainSizer.addButton("Show Card")
 
 class Controller(GUI_Builder):
 	def __init__(self):
@@ -104,7 +248,7 @@ class Controller(GUI_Builder):
 		self.buildGUI()
 		# self.gui.centerWindowAll()
 
-		self.frame_0.showWindow()
+		self.frame_mainMenu.showWindow()
 
 		if __name__ == '__main__':
 			print("GUI Finished Bulding")
